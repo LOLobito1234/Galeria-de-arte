86a1f0be3112bf4a3e16461d65b74f0ee663957c
Jorge123129 <jorgitoeltroll@gmail.com>
2025-11-17 21:59:21 -0600

    estaeslavencida

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Footer from "@/components/footer";
 
 const outfit = Outfit({
   subsets: ["latin"],
